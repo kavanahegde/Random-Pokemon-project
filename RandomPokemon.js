6202d316150fc6c75800da33fdef742266c6a988
KAVANA MANJUNATH HEGDE <79339318+kavanahegde@users.noreply.github.com>
2024-02-14 10:33:51 -0800

    Add files via upload

---
+++ RandomPokemon.js
@@ -0,0 +1,11 @@
+import "./RandomPokemon.css";
+export default function RandomPokemon() {
+    const pokenum = Math.floor(Math.random() * 151) + 1;
+    const url = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokenum}.png`;
+    return (
+        <div className="RandomPokemon">
+        <h1>Pokemon #{pokenum}</h1>
+        <img src={url} alt="" />
+        </div>
+    );
+}  
